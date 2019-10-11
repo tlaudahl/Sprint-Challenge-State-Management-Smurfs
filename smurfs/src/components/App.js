67be6e95e52a492775bf1682d3b26fa5960ea0b7
tlaudahl <travis.laudahl@gmail.com>
2019-10-11 12:23:36 -0500

    MVP Done

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
-class App extends Component {
-  render() {
+import SmurfList from './SmurfList';
+import AddSmurfForm from './AddSmurfForm';
+
+const App = props => {
+
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <SmurfList />
+        <AddSmurfForm />
       </div>
     );
-  }
 }
 
 export default App;
